90edc14541d2a9acaa905b3e1ed6bfb3d9b62781
BocioacaCristian <124626495+BocioacaCristian@users.noreply.github.com>
2023-03-07 21:59:51 +0200

    This is the Unit Testing Part.

--- main.py
+++ main.py
@@ -2,5 +2,19 @@
 import math
 def area_circle(radius):
     area = math.pi * (radius ** 2)
-    return area
+    format_area = "{:.2f}".format(area)
+    float_area = float(format_area)
+    return float_area
 
+print(area_circle(5))
+print(area_circle(10))
+
+def test_area_of_circle():
+    assert (area_circle(5)) == 78.54
+    assert (area_circle(10)) == 314.16
+
+test_area_of_circle()
+
+if __name__ == "__main__":
+    test_area_of_circle()
+    print("Everything passed")
