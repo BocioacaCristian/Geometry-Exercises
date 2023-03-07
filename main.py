e08368021b09949410c2b63bc8e3b2a9ae908ffe
BocioacaCristian <124626495+BocioacaCristian@users.noreply.github.com>
2023-03-07 21:47:51 +0200

    This is the refactory part.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Radius of a circle
+import math
+def area_circle(radius):
+    area = math.pi * (radius ** 2)
+    return area
 
-pi = 3.14
-rad = float(input("Please input the radius of the circle : "))
-area = pi * (rad ** 2)
-print(f"The area of the radius {rad} is {area}")
